5f157b4920f34b5cdf07f3e416fd17ac58286bd4
Tydugs <95362764+Tydugs@users.noreply.github.com>
2021-12-01 21:31:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ơ kìa bạn !',
+    introDesc: `Bạn thấy mình thế nào `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'bạn iu ',
+    desc: 'Phải chăng đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Đẹp zai vl <3 <3',
+    btnNo: 'Không, Bạn xấu vl :3',
+    question:'Sao bạn thấy vậy <3',
+    btnReply: 'Gửi cho Bạn <3',
+    reply: 'Dẹp trai quá trời  <33333333',
+    mess: 'mình biết mà 🥰. thanks nhiều nhiều 😘😘',
+    messDesc: 'Tốí zui zẻ .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
